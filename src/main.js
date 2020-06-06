097270032bd0f5c4f1579ecdda23e4e9825b23af
unknown <hemanths634@gmail.com>
2020-06-06 10:51:12 +0530

    Initial Commit

--- src/main.js
+++ src/main.js
@@ -2,7 +2,12 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-
+import ElementUI from 'element-ui';
+import './plugins/element.js'
+import VueCardFormat from 'vue-credit-card-validation';
+ 
+Vue.use(VueCardFormat);
+Vue.use(ElementUI)
 Vue.config.productionTip = false
 
 new Vue({
